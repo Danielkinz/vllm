d7f5e23e751550259be504e6e29d25a1a7bfcfc7
Daniel <Danielphone221299@gmail.com>
2023-08-11 01:50:40 +0300

    Fixed issue where wheel name wasn't exported to the environment

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cuda_home=/usr/local/cuda-${{ matrix.cuda-version }}
-python_executable=python${{ matrix.python-version }}
+python_executable=python$1
+cuda_home=/usr/local/cuda-$2
 
 # Update paths
 PATH=${cuda_home}/bin:$PATH
@@ -13,10 +13,3 @@ $python_executable -m pip install -r requirements.txt
 
 # Build
 $python_executable setup.py bdist_wheel --dist-dir=dist
-
-# Rename the wheel
-version_suffix=cu${{ matrix.cuda-version }}
-wheel_name=$(ls dist/*whl | xargs -n 1 basename | sed "s/-/-$version_suffix-/2")
-ls dist/*whl | xargs -I {} mv {} ${wheel_name}
-
-echo "wheel_name=${wheel_name}" >> $GITHUB_ENV
