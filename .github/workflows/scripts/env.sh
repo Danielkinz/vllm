80b6680fd29bb76b6c2f85d26000d372da7b40a8
Danielkinz <Danielphone221299@gmail.com>
2023-08-10 23:38:21 +0300

    Update env.sh

--- .github/workflows/scripts/env.sh
+++ .github/workflows/scripts/env.sh
@@ -47,7 +47,7 @@ sudo    apt-get install -y --no-install-recommends \
         nfs-common
 
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install "python$python_version" "python$python_version-distutils"
+sudo apt install "python$python_version" "python$python_version-distutils" "python$python_version-dev"
 wget https://bootstrap.pypa.io/get-pip.py
 python$python_version get-pip.py
 rm get-pip.py
